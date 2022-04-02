0956bce53d6891dc06f4a8c18cbeb9ff1e282452
Stefan Hagen <stefan@hagen.link>
2022-04-02 16:24:47 +0200

    Tests working

--- test/test_cli.py
+++ test/test_cli.py
@@ -2,7 +2,15 @@ import pipa.cli as cli
 
 
 def test_cli_main_empty(capsys):
-    assert cli.main([]) == 0
+    assert cli.main([]) is None
     out, err = capsys.readouterr()
     assert not err
     assert 'pipa' in out
+
+
+def test_cli_main_add_foo(capsys):
+    cli.main(['foo'])
+    out, err = capsys.readouterr()
+    assert not err
+    assert 'pipa' in out and 'foo' in out
+
